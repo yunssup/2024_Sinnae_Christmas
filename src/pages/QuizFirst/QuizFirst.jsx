9c282a5f0e25e8425ded86b73390f0ff3ae897a7
yunssup <btob031541@gmail.com>
2024-12-12 15:27:16 +0900

    feat : 서비스명 수정

--- src/pages/QuizFirst/QuizFirst.jsx
+++ src/pages/QuizFirst/QuizFirst.jsx
@@ -8,7 +8,6 @@ const NextPage = () => {
       <S.Card>
         <S.Header>중고등부 선생님이</S.Header>
         <S.MidHeader>아닌 사람을 고르시오!</S.MidHeader>
-
         <S.GridContainer>
         <S.GridItem><img src="/image/기타성윤.jpg" alt="이미지 1" /></S.GridItem>
         <S.GridItem><img src="/image/동종.jpg" alt="이미지 2" /></S.GridItem>
